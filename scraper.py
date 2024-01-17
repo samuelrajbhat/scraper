63be103a8a7c67fea5ec869066d35007bfd18c53
samue_j <117146342+samuelrajbhat@users.noreply.github.com>
2024-01-17 18:14:27 +0545

    WebScrapper to extract data using a tag and returns python dictonary

---
+++ scraper.py
@@ -0,0 +1,28 @@
+from requests_html import HTMLSession
+
+
+class Scraper():
+    def scrapedata(self, tag):
+        url = f'https://quotes.toscrape.com/tag/{tag}'
+        session = HTMLSession()
+        result = session.get(url)
+        print(result.status_code) 
+
+        qlist = []
+
+        quotes  = result.html.find('div.quote')
+
+        for q in quotes:
+            item = {
+                'text': q.find('span.text', first = True).text.strip(),
+                'Author': q.find('small.author', first = True).text.strip(),
+                }
+            qlist.append(item)
+        
+        return qlist
+    
+quotes = Scraper()
+quotes.scrapedata('life')
+
+
+         
